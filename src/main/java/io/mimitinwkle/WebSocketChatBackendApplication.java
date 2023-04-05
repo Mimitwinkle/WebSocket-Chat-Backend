17128dbc6e545534cdb748110879b617ebb55b13
Kelly <kellygreentea32@gmail.com>
2023-04-05 14:12:35 -0400

    Initial commit

---
+++ src/main/java/io/mimitinwkle/WebSocketChatBackendApplication.java
@@ -0,0 +1,13 @@
+package io.mimitinwkle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketChatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mimitinwkle/WebSocketChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mimitinwkle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
